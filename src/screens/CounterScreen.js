7226ac250c23ac3e1639cbbbaad841f9aaf09f3f
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-08-14 10:59:50 +0500

    Handled counter and color counter with reducer function

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,15 +1,27 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
+const CHANGE_FACTOR = 1;
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case "change_increment":
+            return {...state, counter: state.counter + action.payload};
+        case "change_decrement":
+            return {...state, counter: state.counter - action.payload};
+    }
+}
+
 const CounterScreen = () => {
-    let [counter, setCounter] = useState(0);
+    let [state, dispatch] = useReducer(reducer, {counter: 0});
+    const { counter } = state;
     return(
         <View style = {{marginTop: 10}}>
-            <TouchableOpacity style = {[styles.touchableOpacityStyle,{backgroundColor: "#00ff00"}]} activeOpacity = {0.7} onPress = {() => { setCounter(counter+1) }}>
+            <TouchableOpacity style = {[styles.touchableOpacityStyle,{backgroundColor: "#00ff00"}]} activeOpacity = {0.7} onPress = {() => { dispatch({ type: "change_increment", payload: CHANGE_FACTOR }) }}>
                 <Text style={styles.touchableOpacityTextStyle}>Increase</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style = {[styles.touchableOpacityStyle,{backgroundColor: "#ff0000"}]} activeOpacity = {0.7} onPress = {() => { setCounter(counter-1) } }>
+            <TouchableOpacity style = {[styles.touchableOpacityStyle,{backgroundColor: "#ff0000"}]} activeOpacity = {0.7} onPress = {() => { dispatch({ type: "change_decrement", payload: CHANGE_FACTOR }) } }>
                 <Text style={styles.touchableOpacityTextStyle}>Decrease</Text>
             </TouchableOpacity>
             <Text style = { styles.text }>Counter Screen {counter}</Text>
@@ -22,7 +34,6 @@ const styles = StyleSheet.create({
         fontSize: 30,
       },
       touchableOpacityStyle: {
-        // backgroundColor: '#660000',
         padding:10,
         width:'90%',
         borderRadius: 30,
--- src/screens/squarescreen.js
+++ src/screens/squarescreen.js
@@ -1,31 +1,35 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter'
 
-const CHANGE_FACTOR = 10;
+const CHANGE_FACTOR = 5;
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload <0 ? state : {...state, red: state.red + action.payload};
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload <0 ? state : {...state, green: state.green + action.payload};
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload <0 ? state : {...state, blue: state.blue + action.payload};
+        default:
+            return state;
+    }
+}
 
 const SquareScreen = () => {
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+    const { red, green, blue } = state;
 
-    const setColor = (color, change, callback) => {
-        color + change > 255 || color + change < 0 ? null : callback(color + change);
-    }
 
     return(
         <View>
-            <ColorCounter color = "Red" backgroundColorMore = "rgba(255,0,0,1)" backgroundColorLess = "rgba(255,0,0,0.5)" increase = {() => setColor( red, CHANGE_FACTOR, setRed )} decrease = {() => setColor( red, -1 * CHANGE_FACTOR, setRed )}/>
-            <ColorCounter color = "Green" backgroundColorMore = "rgba(0,255,0,1)" backgroundColorLess = "rgba(0,255,0,0.5)" increase = {() => setColor( green, CHANGE_FACTOR,setGreen )} decrease = {() => setColor( green, -1 * CHANGE_FACTOR, setGreen )}/>
-            <ColorCounter color = "Blue" backgroundColorMore = "rgba(0,0,255,1)" backgroundColorLess = "rgba(0,0,255,0.5)" increase = {() => setColor( blue, CHANGE_FACTOR, setBlue )} decrease = {() => setColor( blue, -1 * CHANGE_FACTOR, setBlue )}/>
+            <ColorCounter color = "Red" backgroundColorMore = "rgba(255,0,0,1)" backgroundColorLess = "rgba(255,0,0,0.5)" increase = {() => dispatch({type: 'change_red',payload: CHANGE_FACTOR})} decrease = {() => dispatch({type: 'change_red',payload: -1 * CHANGE_FACTOR})}/>
+            <ColorCounter color = "Green" backgroundColorMore = "rgba(0,255,0,1)" backgroundColorLess = "rgba(0,255,0,0.5)" increase = {() => dispatch({type: 'change_green',payload: CHANGE_FACTOR})} decrease = {() => dispatch({type: 'change_green',payload: -1 * CHANGE_FACTOR})}/>
+            <ColorCounter color = "Blue" backgroundColorMore = "rgba(0,0,255,1)" backgroundColorLess = "rgba(0,0,255,0.5)" increase = {() => dispatch({type: 'change_blue',payload: CHANGE_FACTOR})} decrease = {() => dispatch({type: 'change_blue',payload: -1 * CHANGE_FACTOR})}/>
             <View style = { { height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` } } />
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    nvStyle: {
-        height: 150, width: 150, backgroundColor: `rgb(0,0,0)`
-    }
-});
 export default SquareScreen;
